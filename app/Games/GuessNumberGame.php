6ca4745518ad1f0a7b2323927bb8cb4b5565f897
YNCBearz <bear19930414@gmail.com>
2021-08-20 23:41:12 +0800

    feat: add help description

--- app/Games/GuessNumberGame.php
+++ app/Games/GuessNumberGame.php
@@ -34,13 +34,13 @@ class GuessNumberGame
 
     private function displayForHelp(): void
     {
-        Brush::paintOnConsole("Description:", ForegroundColors::YELLOW);
+        Brush::paintOnConsole("Description:", ForegroundColors::BROWN);
         Brush::paintOnConsole("  Display help for a command");
         echo PHP_EOL;
-        Brush::paintOnConsole("Options", ForegroundColors::YELLOW);
+        Brush::paintOnConsole("Options", ForegroundColors::BROWN);
         Brush::paintMultiWordsOnConsole(
             [
-                new WordWithColor('  --help', ForegroundColors::GREEN),
+                new WordWithColor('  -h, --help', ForegroundColors::GREEN),
                 new WordWithColor('     Display help for the given command.'),
             ]
         );
