7997780696be05448ac8ecde8c618ddffd400714
YNCBearz <bear19930414@gmail.com>
2021-08-21 15:32:01 +0800

    refactor

--- tests/Utilities/DigitGeneratorTest.php
+++ tests/Utilities/DigitGeneratorTest.php
@@ -32,19 +32,4 @@ class DigitGeneratorTest extends TestCase
         $uniqueNumbers = array_unique($selectedNumbers);
         $this->assertEquals(count($uniqueNumbers), $expected, "4-digit number (repetition): $fourDigits");
     }
-
-    /**
-     * @test
-     */
-    public function Should_Return4Digit_When_Generate4DigitWithoutRepetitions()
-    {
-        $expected = 4;
-
-        $this->sut = new DigitGenerator();
-
-        $actual = $this->sut->generate4DigitWithoutRepetitions();
-
-        $this->assertEquals($expected, strlen((string)$actual));
-    }
-
 }
