b675a432282e043979c1b7d5d3044adf92999bf2
YNCBearz <bear19930414@gmail.com>
2021-09-10 07:11:15 +0800

    feat: project-install.sh

---
+++ project-install.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+# Colors
+red=`tput setaf 1`
+green=`tput setaf 2`
+original=`tput sgr0`
+
+# Copy .env.example to .env
+echo "${green}[Initializing project...] generating .env${original}"
+cp .env.example .env
+echo "${green}[Initializing project...] generating .env success${original}"
