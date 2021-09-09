a248a16c95281d95d8dc2637155b782df5a96d7b
YNCBearz <bear19930414@gmail.com>
2021-09-10 07:17:59 +0800

    feat: project-install.sh (update)

--- project-install.sh
+++ project-install.sh
@@ -9,3 +9,8 @@ original=`tput sgr0`
 echo "${green}[Initializing project...] generating .env${original}"
 cp .env.example .env
 echo "${green}[Initializing project...] generating .env success${original}"
+
+# composer install
+echo "${green}[Initializing project...] composer install${original}"
+composer install
+echo "${green}[Initializing project...] composer install success${original}"
