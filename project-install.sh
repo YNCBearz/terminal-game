25390fedefbcfe25e07ae69e05654f0fced53c88
YNCBearz <bear19930414@gmail.com>
2021-09-10 22:25:09 +0800

    feat: change project-install

--- project-install.sh
+++ project-install.sh
@@ -3,14 +3,15 @@
 # Colors
 red=`tput setaf 1`
 green=`tput setaf 2`
+yellow=`tput setaf 3`
 original=`tput sgr0`
 
 # Copy .env.example to .env
-echo "${green}[Initializing project...] generating .env${original}"
+echo "${yellow}[Initializing project...] generating .env${original}"
 cp .env.example .env
-echo "${green}[Initializing project...] generating .env success${original}"
+echo "${yellow}[Initializing project...] generating .env success${original}"
 
 # composer install
-echo "${green}[Initializing project...] composer install${original}"
+echo "${yellow}[Initializing project...] composer install${original}"
 composer install
-echo "${green}[Initializing project...] composer install success${original}"
+echo "${yellow}[Initializing project...] composer install success${original}"
