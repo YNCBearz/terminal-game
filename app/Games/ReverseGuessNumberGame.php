90b1d0b93dd396c085af772e09d661294e322d8b
YNCBearz <bear19930414@gmail.com>
2021-09-11 23:19:32 +0800

    update

--- app/Games/ReverseGuessNumberGame.php
+++ app/Games/ReverseGuessNumberGame.php
@@ -2,11 +2,14 @@
 
 namespace App\Games;
 
+use App\Enums\Colors\ForegroundColors;
 use App\Games\Contracts\Gameable;
+use App\Utilities\Brush;
 
 class ReverseGuessNumberGame implements Gameable
 {
     protected array $options;
+    protected int $length = 4;
 
     public function __construct(array $options)
     {
@@ -15,6 +18,34 @@ class ReverseGuessNumberGame implements Gameable
 
     public function start()
     {
-        //
+        $this->displayGameInfo();
+        $this->hostGame();
+    }
+
+    private function displayGameInfo()
+    {
+        $length = $this->length;
+
+        Brush::paintOnConsole("Reverse Guess Number ($length-digit)", ForegroundColors::WHITE);
+        echo PHP_EOL;
+        Brush::paintOnConsole("Description:", ForegroundColors::BROWN);
+        Brush::paintOnConsole("  Set a $length-digit secret number, and computer will guess the secret number");
+        echo PHP_EOL;
+    }
+
+    private function hostGame()
+    {
+        //產生所有可能的數字
+
+
+        //While
+        //選一個可能的數字
+
+        //等待User 0A0B
+
+        //過濾可能的數字
+
+
+
     }
 }
