6a34fa3a9a29e2bc5128816ab94fcaa8cbc32c4c
YNCBearz <bear19930414@gmail.com>
2021-09-12 17:50:39 +0800

    feat: fitNumberTestCase

--- tests/Unit/Helpers/ReverseGuessNumberCheckerTest.php
+++ tests/Unit/Helpers/ReverseGuessNumberCheckerTest.php
@@ -11,19 +11,27 @@ class ReverseGuessNumberCheckerTest extends TestCase
 
     /**
      * @test
+     *
+     * @param string $guessNumber
+     * @param string $guessResult
+     * @param string $number
+     *
+     * @dataProvider fitNumberTestCase
      */
-    public function GivenFitNumber_WhenIsFitNumber_ReturnTrue()
+    public function GivenFitNumber_WhenIsFitNumber_ReturnTrue(string $guessNumber, string $guessResult, string $number)
     {
-        $guessNumber = 0;
-        $guessResult = '0A0B';
-
-        $number = '1';
-
         $this->sut = new ReverseGuessNumberChecker($guessNumber, $guessResult);
 
         $actual = $this->sut->isFitNumber($number);
 
-        $this->assertTrue($actual);
+        $this->assertTrue($actual, "Fit number: $number (guess number: $guessNumber | guess result: $guessResult)");
+    }
+
+    public function fitNumberTestCase(): array
+    {
+        return [
+            ['1', '0A0B', '0'],
+        ];
     }
 
 }
