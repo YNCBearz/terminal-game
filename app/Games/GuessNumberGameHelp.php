1acdc63e5572323ef7e95af87c20fcd773fdfe92
YNCBearz <bear19930414@gmail.com>
2021-09-12 20:13:41 +0800

    feat: update gamehelp

--- app/Games/GuessNumberGameHelp.php
+++ app/Games/GuessNumberGameHelp.php
@@ -26,7 +26,9 @@ class GuessNumberGameHelp
                 new WordWithColor("  -h, --help", ForegroundColors::GREEN),
                 new WordWithColor("         Display help for the given command. \n"),
                 new WordWithColor("  -l, --length", ForegroundColors::GREEN),
-                new WordWithColor("       Setting for the digit number (default: 4)."),
+                new WordWithColor("       Setting for the digit number (default: 4). \n"),
+                new WordWithColor("  -r, --reverse", ForegroundColors::GREEN),
+                new WordWithColor("      Setting for the reverse guess game. \n"),
             ],
         );
     }
